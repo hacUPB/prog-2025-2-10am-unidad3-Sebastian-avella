00b85c7009401b529c7548384f18585d408a5dd8
sebastian avella calderon <sebastian.avella@upb.edu.co>
2025-08-21 11:04:34 -0500

    SRC

---
+++ SRC/Ejercicio1.py
@@ -0,0 +1,17 @@
+print ("Ingresa tu nombre y apellido")
+nombre = input ()
+print ("Bienvenido: ")
+#Opcion 2
+print (nombre)
+#Opcion 2
+print ("Bienvenido: ", nombre)
+#Calcular el IMC de esa persona
+#Leer peso, altura
+peso = input("Ingresa tu peso en kilogramos: ")
+peso = float(peso)
+altura = input (" Ingresa tu talla en metros: ")
+altura = float(altura)
+imc = peso/altura**2
+#Mostrar imc
+print("Tu IMC = ", imc)
+
