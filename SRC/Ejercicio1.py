a568ca2dfbc8a27300931eed1b8671e47006a457
sebastian avella calderon <sebastian.avella@upb.edu.co>
2025-08-26 10:38:26 -0500

    SRC

--- SRC/Ejercicio1.py
+++ SRC/Ejercicio1.py
@@ -27,3 +27,17 @@ elif imc >= 40:
     print("obesidad extrema 3")
 
     
+
+
+if imc < 18.5:
+    mensaje = "bajo peso"
+elif imc < 25:
+    mensaje = "peso normal"
+elif imc < 35:
+    mensaje = "obesidad tipo 1"
+elif imc < 40:
+    mensaje = "obesidad tipo 2"
+else:
+    mensaje = "obesidad extrema"
+
+print(f"Paciente {nombre}, tiene un imc de {imc:0.2f} y su condicion es {mensaje}. ") 
