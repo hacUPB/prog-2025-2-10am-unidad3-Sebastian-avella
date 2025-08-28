8710af80aecad07b4a314011cba590cd87994754
sebastian avella calderon <sebastian.avella@upb.edu.co>
2025-08-28 11:32:15 -0500

    SRC

--- SRC/calculadora.py
+++ SRC/calculadora.py
@@ -1,4 +1,5 @@
 # calculadora
+'''
 control = True #variable booleana
 while control == True:
     num1 = int(input ("Ingrese el primer numero"))
@@ -20,6 +21,36 @@ while control == True:
             resultado = num1 / num2
         case "E":
             control = False
+        case _:
+            print ("Modo no válido")
+    print (f"Resultado = {resultado}")
+'''
+
+#control = True #variable booleana
+while True:
+    num1 = int(input ("Ingrese el primer numero"))
+    num2 = int(input ("Ingrese el segundo numero"))
+    print ("S. sumar\nR. Restar\nM. Multilicar\nD Dividir\nE. Exponencial\nF. Salir")
+    opcion = input("Elija una opcion")
+    opcion = opcion.upper() #Se convierte el texto a mayúscula
+    match opcion:
+        case "S":
+            print("Sumar")
+            resultado = num1 + num2
+        case "R":
+            print("Restar")
+            resultado = num1 - num2
+        case "M":
+            print("Multiplicar")
+            resultado = num1 * num2
+        case "D":
+            print("Dividir")
+            resultado = num1 / num2
+        case "E":
+            print("Exponencial")
+            resultado = num1 ** num2
+        case "F":
+            break
         case _:
             print ("Modo no válido")
     print (f"Resultado = {resultado}")
